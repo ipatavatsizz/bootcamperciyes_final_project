b18a95722deb5ed2a9edbea3d488330f2102536b
ipatavatsizz <ankatryusuf@hotmail.com>
2023-12-05 12:13:43 +0300

    LocaleKeys updated

---
+++ lib/product/constant/locale_keys.g.dart
@@ -0,0 +1,15 @@
+// DO NOT EDIT. This is code generated via package:easy_localization/generate.dart
+
+abstract class  LocaleKeys {
+  static const title = 'title';
+  static const subtitle = 'subtitle';
+  static const language = 'language';
+  static const loading = 'loading';
+  static const getting_data = 'getting_data';
+  static const getting_location_data = 'getting_location_data';
+  static const errors_unexpected = 'errors.unexpected';
+  static const errors_something_went_wrong = 'errors.something_went_wrong';
+  static const errors = 'errors';
+  static const search = 'search';
+
+}
