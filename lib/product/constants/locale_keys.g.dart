820376f18301465b0ab3c0499d4c78a1d7f96f78
ipatavatsizz <ankatryusuf@hotmail.com>
2023-12-05 12:14:31 +0300

    Changed file path

--- lib/product/constants/locale_keys.g.dart
+++
@@ -1,7 +0,0 @@
-// DO NOT EDIT. This is code generated via package:easy_localization/generate.dart
-
-abstract class  LocaleKeys {
-  static const title = 'title';
-  static const language = 'language';
-
-}
