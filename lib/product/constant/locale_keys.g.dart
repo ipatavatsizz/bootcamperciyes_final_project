99f9430ac719d70a0921fe225c969eafdbb20903
ipatavatsizz <ankatryusuf@hotmail.com>
2023-12-07 01:25:06 +0300

    Update locale_keys.g.dart

--- lib/product/constant/locale_keys.g.dart
+++ lib/product/constant/locale_keys.g.dart
@@ -4,12 +4,25 @@ abstract class  LocaleKeys {
   static const title = 'title';
   static const subtitle = 'subtitle';
   static const language = 'language';
+  static const licenses = 'licenses';
   static const loading = 'loading';
   static const getting_data = 'getting_data';
   static const getting_location_data = 'getting_location_data';
+  static const yes = 'yes';
+  static const no = 'no';
+  static const unknown = 'unknown';
   static const errors_unexpected = 'errors.unexpected';
   static const errors_something_went_wrong = 'errors.something_went_wrong';
+  static const errors_location_service_disabled = 'errors.location_service_disabled';
   static const errors = 'errors';
   static const search = 'search';
+  static const search_terms_delivery = 'search_terms.delivery';
+  static const search_terms_rating = 'search_terms.rating';
+  static const search_terms_nationalPhoneNumber = 'search_terms.nationalPhoneNumber';
+  static const search_terms = 'search_terms';
+  static const select_language = 'select_language';
+  static const change_text = 'change_text';
+  static const connection = 'connection';
+  static const no_connection = 'no_connection';
 
 }
