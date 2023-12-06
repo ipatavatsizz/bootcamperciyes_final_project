19572c6993187026b0f54ceb4d05c6145ec00942
ipatavatsizz <ankatryusuf@hotmail.com>
2023-12-07 01:24:17 +0300

    Added databaseservice and firebase- with initializing

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,15 @@
 import 'package:bootcamperciyes_final_project/feature/not_found/not_found_view.dart';
 import 'package:bootcamperciyes_final_project/feature/splash/splash_view.dart';
+import 'package:bootcamperciyes_final_project/firebase_options.dart';
 import 'package:bootcamperciyes_final_project/product/constant/application_constant.dart';
 import 'package:bootcamperciyes_final_project/product/constant/language_constant.dart';
-import 'package:bootcamperciyes_final_project/product/cubit/places/places_cubit.dart';
+import 'package:bootcamperciyes_final_project/product/cubit/card_cubit.dart';
+import 'package:bootcamperciyes_final_project/product/cubit/language_cubit.dart';
+import 'package:bootcamperciyes_final_project/product/cubit/places_cubit.dart';
+import 'package:bootcamperciyes_final_project/product/cubit/search_cubit.dart';
+import 'package:bootcamperciyes_final_project/product/service/database_service.dart';
 import 'package:easy_localization/easy_localization.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -12,7 +18,19 @@ import 'package:google_fonts/google_fonts.dart';
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
+  await SystemChannels.textInput.invokeMethod('TextInput.hide');
+
   await EasyLocalization.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  // This feature will be added later
+  // final GoogleSignIn googleSignIn = GoogleSignIn(
+  //   // The OAuth client id of your app. This is required.
+  //   clientId: 'Your Client ID',
+  // );
 
   await SystemChrome.setPreferredOrientations(
     [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown],
@@ -20,16 +38,22 @@ Future<void> main() async {
 
   await SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersiveSticky);
 
+  await DatabaseService.initialize();
+
   runApp(
     MultiBlocListener(
       listeners: [
-        BlocProvider<PlacesCubit>(
-          create: (context) => PlacesCubit(),
-        ),
+        BlocProvider<PlacesCubit>(create: (context) => PlacesCubit()),
+        BlocProvider<SearchCubit>(create: (context) => SearchCubit()),
+        BlocProvider<CardCubit>(create: (context) => CardCubit()),
+        BlocProvider<LanguageCubit>(create: (context) => LanguageCubit()),
       ],
       child: EasyLocalization(
         path: Application.path.translations,
-        startLocale: Application.language.toLocale(),
+        startLocale: Languages.fromString(
+          DatabaseService.instance.database
+              .get('language', defaultValue: Application.language.name)!,
+        ).toLocale(),
         fallbackLocale: Languages.english.toLocale(),
         supportedLocales: Application.supportedLanguages.toLocale(),
         child: MainApp(),
@@ -56,6 +80,10 @@ class MainApp extends StatelessWidget {
       home: SplashView(),
       theme: ThemeData.light().copyWith(
         brightness: Brightness.light,
+        appBarTheme: Theme.of(context).appBarTheme.copyWith(
+              elevation: 0,
+              scrolledUnderElevation: 0,
+            ),
         textTheme: GoogleFonts.openSansTextTheme(),
       ),
       // darkTheme: ThemeData.dark().copyWith(
