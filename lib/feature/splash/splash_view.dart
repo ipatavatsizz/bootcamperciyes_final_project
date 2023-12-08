6ab6373bbfd9e2231f19a3fd07e3a4d2b8d83d69
ipatavatsizz <ankatryusuf@hotmail.com>
2023-12-08 23:24:38 +0300

    After onboard, changed a bit

--- lib/feature/splash/splash_view.dart
+++ lib/feature/splash/splash_view.dart
@@ -1,12 +1,10 @@
 import 'package:animated_text_kit/animated_text_kit.dart';
-import 'package:bootcamperciyes_final_project/product/constant/application_constant.dart';
+import 'package:bootcamperciyes_final_project/feature/onboard/onboard_item.dart';
+import 'package:bootcamperciyes_final_project/feature/onboard/onboard_view.dart';
 import 'package:bootcamperciyes_final_project/product/constant/locale_keys.g.dart';
-import 'package:bootcamperciyes_final_project/product/constant/navigation_constant.dart';
-import 'package:bootcamperciyes_final_project/product/cubit/auth_cubit.dart';
+import 'package:bootcamperciyes_final_project/product/core/application.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:geolocator/geolocator.dart';
 
 class SplashView extends StatefulWidget {
   const SplashView({super.key});
@@ -17,14 +15,43 @@ class SplashView extends StatefulWidget {
 
 class _SplashViewState extends State<SplashView> {
   Future<void> initialize() async {
-    Future.wait([
-      Geolocator.requestPermission(),
-      context.read<AuthCubit>().signIn(),
-    ]);
+    // Future.wait([
+    //   Geolocator.requestPermission(),
+    //   context.read<AuthCubit>().signIn(),
+    // ]);
+
+    debugPrint(Application.path.images.add('welcome.png'));
 
     Application.navigation.currentState?.pushReplacement(
       PageRouteBuilder(
-        pageBuilder: (context, _, __) => NavigationPages.home.page,
+        pageBuilder: (context, _, __) => OnboardView(
+          children: [
+            OnboardItem(
+              image: Image.asset(
+                Application.path.images.add('welcome.png'),
+                width: 200,
+                height: 200,
+              ),
+              onNext: (controller) => controller.nextPage(
+                duration: Duration(milliseconds: 250),
+                curve: Curves.linear,
+              ),
+              text: [
+                'Merhaba! Görüyorum ki bu ${LocaleKeys.title.tr()} ile ilk deneyimin.',
+                'Hadi, izin ver sana uygulamayı tanıtayım.',
+              ],
+            ),
+            OnboardItem(
+              onFinish: (controller) async {},
+              onNext: (controller) {},
+              text: [
+                'Öncelikle birkaç adet uygulama izni istemem gerekecek.',
+                'Bu izinleri sadece uygulama çalışırken kullandığımı unutma.',
+                'Veri güvenliğin konusunda bana güvenebilirsin! 😊',
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
